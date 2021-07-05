f82f29f948625f98f222e645b4822d7a0996ef64
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-05 11:38:27 +0530

    descending order

--- Sorting.sh
+++ Sorting.sh
@@ -10,5 +10,10 @@ dict[res4]=$((a*b+c));
 arr=();
 arr=(${dict[res1]} ${dict[res2]} ${dict[res3]} ${dict[res4]});
 
-readarray -t sorted < <(for a in "${arr[@]}"; do echo "$a"; done | sort -n)
+
 readarray -t sorted2 < <(for a in "${arr[@]}"; do echo "$a"; done | sort -nr)
+
+
+echo "Descending Order : "${sorted2[@]};
+echo "Arr = "${arr[@]};
+#echo ${dict[@]};
