35b5ab61d5d57d5dff8b4ed44bb8440a4f09b210
Bathala-Haresh <hareshbathala87@gmail.com>
2021-07-05 11:35:21 +0530

    reading array

--- Sorting.sh
+++ Sorting.sh
@@ -9,3 +9,6 @@ dict[res3]=$((a%b+c));
 dict[res4]=$((a*b+c));
 arr=();
 arr=(${dict[res1]} ${dict[res2]} ${dict[res3]} ${dict[res4]});
+
+readarray -t sorted < <(for a in "${arr[@]}"; do echo "$a"; done | sort -n)
+readarray -t sorted2 < <(for a in "${arr[@]}"; do echo "$a"; done | sort -nr)
